6e6260cb7c77c6344a72f27fc782456d257faa96
Benoit Dumas <benoitdudu38@hotmail.fr>
2022-10-16 15:28:13 +0200

    set the response status to not block qemu

--- src/target_systemc.cpp
+++ src/target_systemc.cpp
@@ -7,5 +7,6 @@ target_systemc::target_systemc(sc_core::sc_module_name name)
 }
 
 void target_systemc::b_transport( tlm::tlm_generic_payload& gp, sc_core::sc_time& delay) {
-    printf("received transaction from QEMU \\o/\n");
+    printf("received %s transaction from QEMU \\o/\n", gp.is_read() ? "READ " : "WRITE");
+    gp.set_response_status(tlm::TLM_OK_RESPONSE);
 }
